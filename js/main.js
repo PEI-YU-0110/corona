1e0729cd8eb8b14b19f2d7b39cd8676cc100851a
PEI-YU-0110 <61971750+PEI-YU-0110@users.noreply.github.com>
2020-06-09 22:33:42 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("9 June 2020 9:56:00 GMT+01:00");			
+		var endTime = new Date("15 July 2020 00:00:00 GMT+08:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
